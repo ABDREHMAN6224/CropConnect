dcff0777757e1f424f57ee2d52dc598cc8d2af54
Faakhir30 <zahidfaakhir@gmail.com>
2024-03-02 12:20:14 +0500

    marketplace

---
+++ backend/routes/marketplace.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { isAdmin, protect } from '../middlewares/auth.js';
+import { createMarketplace, updateMarketplace, getMarketplace, getMarketplaces, deleteMarketplace, buyMarketplace,  } from '../controllers/marketplace.js';
+
+const router = express.Router();
+
+router.post('/marketplace', protect, createMarketplace);
+router.get('/marketplaces', getMarketplaces);
+router.get('/marketplace/:id', getMarketplace);
+router.put('/marketplace/:id', protect, updateMarketplace);
+router.delete('/marketplace/:id', protect, deleteMarketplace);
+router.put('/marketplace/buy/:id', protect, buyMarketplace);

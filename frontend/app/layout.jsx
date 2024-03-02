135ee367df0ee989ec66c533cd7b8a74a921232b
Hamza <muhammadhamzam1486@gmail.com>
2024-03-02 14:47:39 +0500

    remove stash comments

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,11 +1,7 @@
 import { Inter } from "next/font/google";
 import "react-toastify/dist/ReactToastify.css";
-<<<<<<< Updated upstream
 import "/app/globals.css";
-=======
-import "@/app/globals.css";
 import { SocketProvider } from "./context/socketContext";
->>>>>>> Stashed changes
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} dark:bg-gray-900 dark:text-white`}>
-        <SocketProvider>
-        {children}
-        </SocketProvider>
+        <SocketProvider>{children}</SocketProvider>
       </body>
     </html>
   );

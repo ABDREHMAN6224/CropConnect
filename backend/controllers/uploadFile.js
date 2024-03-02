0b44957ffc6c762fa2749ab23ea4a5952f62ac39
Hamza <muhammadhamzam1486@gmail.com>
2024-03-02 16:10:16 +0500

    fix upload issues

--- backend/controllers/uploadFile.js
+++ backend/controllers/uploadFile.js
@@ -1,11 +1,11 @@
-import ExpressAsyncHandler from 'express-async-handler';
-import User from '../model/user.js';
+import ExpressAsyncHandler from "express-async-handler";
+import User from "../model/user.js";
 
 export const uploadFile = async (req, res) => {
-    const urlServer = process.env.URL_SERVER
-    if (!req.file) {
-        return res.status(404).json({ err: "no file found" })
-    }
-    const url = `${urlServer}/${req.file.filename}`
-    return res.status(200).json({ url: url })
-}
+  const urlServer = process.env.UPLOAD_PATH;
+  if (!req.file) {
+    return res.status(404).json({ err: "no file found" });
+  }
+  const url = `${urlServer}/${req.file.filename}`;
+  return res.status(200).json({ url: url });
+};

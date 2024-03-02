81e67fb5681e4969fff4430b4bab28e9cbfc4b85
Faakhir30 <zahidfaakhir@gmail.com>
2024-03-02 10:12:29 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { Server } from 'socket.io';
-import path from 'path';
+import dotenv from "dotenv"
+import express from "express"
+import bodyParser from "body-parser";
+import cors from "cors"
+import { fileURLToPath } from "url";
+import path from "path";
 
-dotenv.config();
 
-const __dirname =  dirname(fileURLToPath(import.meta.url));
-
+dotenv.config()
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
-
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-    }
-});
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
-});
-
+app.use(cors({
+    origin:["https://archats-arm.netlify.app","http://localhost:5173"],
+    credentials:true
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public/")))

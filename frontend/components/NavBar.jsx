fd3ba6bdcf765052df351c9199f246fa3ca060b4
Hamza <muhammadhamzam1486@gmail.com>
2024-03-02 16:53:19 +0500

    add link for user page

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -95,7 +95,7 @@ export default function NavBar() {
             </li>
             <li>
               <Link
-                href="#"
+                href="/user/profile"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-600 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 <FaUser className="text-2xl" />

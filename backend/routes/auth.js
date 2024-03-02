533b5f92d3b0a772a3c94b8bb6a59ed43d36c8d1
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2024-03-02 11:49:20 +0500

    merge confolt resolve

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,13 +11,8 @@ router.get('/user/:id',protect, getUser);
 router.put('/user/:id',protect, updateUser);
 router.put('/user/uploadAvatar/:id',protect, uploadAvatar);
 router.delete('/user/:id',protect, deleteUser);
-<<<<<<< Updated upstream
 router.post("/user/resetPassword/:id",protect, resetPassword);
 router.post("/resetPassword/:resetPasswordToken/:id", resetPassword);
 
 
 export default router;
-=======
-
-export default router;
->>>>>>> Stashed changes
